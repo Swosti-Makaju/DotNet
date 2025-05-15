21d89d28fc703f30bf106ad48995b1afdee5672a
Swosti <138574437+Swosti-Makaju@users.noreply.github.com>
2025-05-15 17:04:42 +0545

    lab

---
+++ lab2MDI/lab2MDI/MDIParentForm.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab2MDI
+{
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ChildForm child = new ChildForm();
+            child.MdiParent = this; // Set the parent to this MDI form
+            child.Text = "Child Window " + this.MdiChildren.Length;
+            child.Show(); // Show the child form inside the MDI parent
+
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
