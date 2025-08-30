1adb933c356803f75b16934d62aaa48ebf527046
Swosti <138574437+Swosti-Makaju@users.noreply.github.com>
2025-08-30 20:36:43 +0545

    lab work

---
+++ BaseKeyword-swosti-33/BaseKeyword-swosti-33/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseKeyword_swosti_33
+{
+    class Animal
+    {
+        public Animal()
+        {
+            Console.WriteLine("Animal constructor called.");
+        }
+
+        public void Eat()
+        {
+            Console.WriteLine("Animal is eating.");
+        }
+    }
+
+    // Derived class
+    class Dog : Animal
+    {
+        public Dog() : base()  // Calls base class constructor
+        {
+            Console.WriteLine("Dog constructor called.");
+        }
+
+        public void ShowBehavior()
+        {
+            base.Eat();  // Calls base class method
+            Console.WriteLine("Dog is barking.");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog myDog = new Dog();
+            myDog.ShowBehavior();
+        }
+    }
+}
---
+++ binary-operator-swosti-33/binary-operator-swosti-33/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace binary_operator_swosti_33
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculation a = new Calculation(10);
+            Calculation b = new Calculation(5);
+
+            Calculation sum = a + b;
+            Console.WriteLine("a + b = " + sum.Display());  // Output: 15
+
+            Calculation diff = a - b;
+            Console.WriteLine("a - b = " + diff.Display());  // Output: 5
+
+            Calculation product = a * b;
+            Console.WriteLine("a * b = " + product.Display());  // Output: 50
+
+            Calculation quotient = a / b;
+            Console.WriteLine("a / b = " + quotient.Display());  // Output: 2
+            Calculation reminder = a % 2;
+            Console.WriteLine("a % 2 = " + reminder.Display());  // Output: 0
+
+            Console.ReadLine();
+
+        }
+    }
+    public class Calculation
+    {
+        int x;
+        public Calculation(int x)
+        {
+            this.x = x;
+        }
+        // Binary + operator
+        public static Calculation operator +(Calculation a, Calculation b)
+        {
+            return new Calculation(a.x + b.x);
+        }
+
+        // Binary - operator
+        public static Calculation operator -(Calculation a, Calculation b)
+        {
+            return new Calculation(a.x - b.x);
+        }
+
+        // Binary * operator
+        public static Calculation operator *(Calculation a, Calculation b)
+        {
+            return new Calculation(a.x * b.x);
+        }
+
+        // Binary / operator
+        public static Calculation operator /(Calculation a, Calculation b)
+        {
+            return new Calculation(a.x / b.x);
+        }
+        public static Calculation operator %(Calculation a, int scalar)
+        {
+            return new Calculation(a.x % scalar);
+        }
+
+        public int Display()
+        {
+            return x;
+        }
+
+    }
+
+}
