599ee76c6d38c338e70828fa715713898a68edd5
Swosti <138574437+Swosti-Makaju@users.noreply.github.com>
2025-08-31 13:24:33 +0545

    labwork

---
+++ vowel&consonent-swosti-33/vowel&consonent-swosti-33/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter a string:");
+        string input = Console.ReadLine();
+        string vowels = "";
+        string consonants = "";
+        foreach (char c in input)
+        {
+            if (char.IsLetter(c))
+            {
+                char lowerChar = char.ToLower(c);
+                if ("aeiou".Contains(lowerChar))
+                    vowels += c;
+                else
+                    consonants += c;
+            }
+            File.WriteAllText("VOWEL.TXT", vowels);
+            File.WriteAllText("CONSONANT.TXT", consonants);
+            Console.WriteLine("Vowels written to VOWEL.TXT");
+            Console.WriteLine("Consonants written to CONSONANT.TXT");
+        }
+    }
+}
