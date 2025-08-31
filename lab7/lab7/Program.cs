65d3dfb92c7806eab7e6e46bc5abe54a081be76a
Swosti <138574437+Swosti-Makaju@users.noreply.github.com>
2025-08-31 12:58:48 +0545

    labwork

---
+++ lab7/lab7/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace ConsoleApp101
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter rows: ");
+            int m = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter columns: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[,] first = new int[m, n];
+            int[,] second = new int[m, n];
+            int[,] resultant = new int[m, n];
+
+            Console.WriteLine("Enter elements of First Matrix:");
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write($"First[{i},{j}]: ");
+                    first[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine("Enter elements of Second Matrix:");
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write($"Second[{i},{j}]: ");
+                    second[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            // Performing Matrix addition
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    resultant[i, j] = first[i, j] + second[i, j];
+                }
+            }
+
+            Console.WriteLine("Resultant Matrix:");
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(resultant[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
---
+++ lab8/lab8/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+class MatrixAdditionJagged
+{
+    static void Main()
+    {
+        Console.Write("Enter number of rows: ");
+        int rows = int.Parse(Console.ReadLine());
+
+        // Declare three jagged arrays
+        int[][] matrixA = new int[rows][];
+        int[][] matrixB = new int[rows][];
+        int[][] resultMatrix = new int[rows][];
+
+        // Input matrix dimensions and elements
+        for (int i = 0; i < rows; i++)
+        {
+            Console.Write($"Enter number of columns for row {i + 1}: ");
+            int cols = int.Parse(Console.ReadLine());
+
+            matrixA[i] = new int[cols];
+            matrixB[i] = new int[cols];
+            resultMatrix[i] = new int[cols];
+
+            Console.WriteLine($"Enter elements for Matrix A, Row {i + 1}:");
+            for (int j = 0; j < cols; j++)
+            {
+                Console.Write($"A[{i}][{j}] = ");
+                matrixA[i][j] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine($"Enter elements for Matrix B, Row {i + 1}:");
+            for (int j = 0; j < cols; j++)
+            {
+                Console.Write($"B[{i}][{j}] = ");
+                matrixB[i][j] = int.Parse(Console.ReadLine());
+
+                // Add and store in result matrix
+                resultMatrix[i][j] = matrixA[i][j] + matrixB[i][j];
+            }
+        }
+
+        // Display all three matrices
+        Console.WriteLine("\nMatrix A:");
+        PrintMatrix(matrixA);
+
+        Console.WriteLine("\nMatrix B:");
+        PrintMatrix(matrixB);
+
+        Console.WriteLine("\nResult (A + B):");
+        PrintMatrix(resultMatrix);
+    }
+
+    static void PrintMatrix(int[][] matrix)
+    {
+        for (int i = 0; i < matrix.Length; i++)
+        {
+            foreach (int val in matrix[i])
+            {
+                Console.Write(val + "\t");
+            }
+            Console.WriteLine();
+        }
+    }
+}
