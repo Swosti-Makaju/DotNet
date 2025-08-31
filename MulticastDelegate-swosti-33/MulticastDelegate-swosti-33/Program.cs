db41770241d134c31f0d340e870c7b47d212cc6c
Swosti <138574437+Swosti-Makaju@users.noreply.github.com>
2025-08-31 13:09:23 +0545

    labwork

---
+++ MulticastDelegate-swosti-33/MulticastDelegate-swosti-33/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MulticastDelegate_swosti_33
+{
+    public delegate void MathOperation(int x, int y);
+
+    class Program
+    {
+        // Method to add two numbers
+        public static void Add(int a, int b)
+        {
+            Console.WriteLine("Addition: {0} + {1} = {2}", a, b, a + b);
+        }
+
+        // Method to subtract two numbers
+        public static void Subtract(int a, int b)
+        {
+            Console.WriteLine("Subtraction: {0} - {1} = {2}", a, b, a - b);
+        }
+
+        static void Main(string[] args)
+        {
+            // Create delegate instance and assign methods
+            MathOperation operation = Add;        // Add first
+            operation += Subtract;               // Then subtract
+
+            // Call the multicast delegate
+            Console.WriteLine("Enter two numbers:");
+
+            Console.Write("First number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Second number: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\nPerforming operations using multicast delegate:\n");
+            operation(num1, num2);  // Calls both Add and Subtract
+        }
+    }
+}
---
+++ multipleinheritence-using-interface-swosti-33/multipleinheritence-using-interface-swosti-33/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace multipleinheritence_using_interface_swosti_33
+{
+    interface IPrintable
+    {
+        void Print();
+    }
+
+    // Second interface
+    interface IScannable
+    {
+        void Scan();
+    }
+
+    // Class implementing both interfaces
+    class MultiFunctionPrinter : IPrintable, IScannable
+    {
+        public void Print()
+        {
+            Console.WriteLine("Printing document...");
+        }
+
+        public void Scan()
+        {
+            Console.WriteLine("Scanning document...");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MultiFunctionPrinter mfp = new MultiFunctionPrinter();
+
+            Console.WriteLine("Demonstrating Multiple Inheritance using Interfaces:\n");
+            mfp.Print();
+            mfp.Scan();
+
+        }
+    }
+}
